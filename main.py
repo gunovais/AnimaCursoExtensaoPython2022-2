3d747f0265850859cc288333c1aec202f7e4a538
Gustavo Pereira <118381250+gunovais@users.noreply.github.com>
2022-11-16 20:54:28 +0000

    Adicionado mais alguns comandos

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ print("Alo mundo!")
 nome = "Gustavo Novais"
 idade = 22
 print(nome)
-print("Minha idade é "+str(idade))
+print("Minha idade é "+str(idade)+" anos")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+print(f"Boa noite, {nome}")
+print("Sua idade é {}".format(idade))
+dobro = idade*2
+print("O dobro da sua idade é {}".format(dobro))
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
+else:
+  print("Você não pode beber e nem dirigir")
